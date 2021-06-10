2dc9e524bcb339b1c47372cce07294a0ee433eaa
WithoutLogic <51289664+WithoutLogic@users.noreply.github.com>
2021-06-10 10:49:35 +0800

    fix

--- index.js
+++ index.js
@@ -1,51 +1,41 @@
 module.exports = function antiBodyBlock(mod) {
   let timerInterval = null;
-  let partyMembers = [],
-    unk1 = 0,
-    unk2 = 0,
-    unk3 = 0;
+  let partyMembers = [];
+  let enabled = true;
 
   mod.game.initialize('me');
   mod.game.initialize('party');
 
   const removeUser = (e) => {
-    if (!partyMembers.length) {
-      return;
-    }
-    let idx = partyMembers.findIndex(
-      (x) => x.playerId === e.playerId && x.serverId === e.serverId
-    );
-    if (idx > -1) {
-      partyMembers.splice(idx, 1);
-    }
+    if (!partyMembers.length) { return; }
+    let idx = partyMembers.findIndex((x) => x.playerId === e.playerId && x.serverId === e.serverId);
+    if (idx > -1) { partyMembers.splice(idx, 1); }
   };
 
   mod.game.party.on('leave', () => {
     partyMembers = [];
-    (unk1 = 0), (unk2 = 0), (unk3 = 0);
   });
 
   mod.game.party.on('member_kick', removeUser);
   mod.game.party.on('member_leave', removeUser);
 
+  /*mod.hook('S_PARTY_INFO', 2, (event) => {
+    console.log('S_PARTY_INFO > gameId: ' + event.gameId + ', partyId: ' + event.partyId + ', raid: ' + event.raid);
+  });*/
+
   const removeBodyBlock = () => {
-    if (!partyMembers.length) {
-      return;
-    }
+    if (!partyMembers.length) { return; }
+	if (!enabled) { return; }
     if (mod.game.me.inDungeon && mod.game.party.inParty) {
       partyMembers.forEach((mem) => {
         if (mem.online) {
-          if (mem.gameId == 0) {
-            return;
-          }
-          /*mod.send('S_PARTY_INFO', 1, {
-            leader: mem.gameId,
-            unk1: unk1,
-            unk2: unk2,
-            unk3: unk3,
-            unk4: 1,
-          });*/
-		  console.log('Due to recent update, abb is broken - use fps mode 3 instead and delete or turn off this mod');
+          if (mem.gameId == 0) { return; }
+          //console.log('abb > gameId: ' + mem.gameId + ', partyId: ' + mem.partyId);
+          mod.send('S_PARTY_INFO', 2, {
+            gameId: mem.gameId,
+            partyId: mem.partyId,
+            raid: true,
+          });
         }
       });
     }
@@ -53,30 +43,19 @@ module.exports = function antiBodyBlock(mod) {
 
   mod.hook('S_LOGIN', 14, (event) => {
     partyMembers = [];
-    (unk1 = 0), (unk2 = 0), (unk3 = 0);
+    mod.clearAllIntervals();
     timerInterval = mod.setInterval(removeBodyBlock, 5000);
   });
 
-  mod.hook('S_PARTY_MEMBER_LIST', 7, (event) => {
-    if (event.raid) {
-      return;
-    }
-    let idx = -1,
-      gId = -1n;
-    unk1 = event.unk2;
-    unk2 = event.unk3;
-    unk3 = event.unk4;
+  mod.hook('S_PARTY_MEMBER_LIST', 9, (event) => {
+    if (event.raid) { return; }
+    let idx = -1, gId = -1n;
     event.members.forEach((mem) => {
-      idx = partyMembers.findIndex(
-        (x) => x.playerId === mem.playerId && x.serverId === mem.serverId
-      );
+      idx = partyMembers.findIndex((x) => x.playerId === mem.playerId && x.serverId === mem.serverId);
       if (idx === -1) {
         gId = mem.gameId;
         if (gId === 0n) {
-          if (
-            mod.game.me.playerId === mem.playerId &&
-            mod.game.me.serverId === mem.serverId
-          ) {
+          if (mod.game.me.playerId === mem.playerId && mod.game.me.serverId === mem.serverId) {
             gId = mod.game.me.gameId;
           }
         }
@@ -85,6 +64,7 @@ module.exports = function antiBodyBlock(mod) {
           playerId: mem.playerId,
           serverId: mem.serverId,
           online: mem.online,
+          partyId: event.id,
         });
       }
     });
@@ -92,14 +72,9 @@ module.exports = function antiBodyBlock(mod) {
 
   mod.hook('S_SPAWN_USER', 17, (event) => {
     let n = partyMembers.length;
-    if (!n) {
-      return;
-    }
+    if (!n) { return; }
     for (let i = 0; i < n; i++) {
-      if (
-        partyMembers[i].playerId === event.playerId &&
-        partyMembers[i].serverId === event.serverId
-      ) {
+      if (partyMembers[i].playerId === event.playerId && partyMembers[i].serverId === event.serverId) {
         partyMembers[i].gameId = event.gameId;
         partyMembers[i].online = true;
       }
@@ -108,16 +83,20 @@ module.exports = function antiBodyBlock(mod) {
 
   mod.hook('S_LOGOUT_PARTY_MEMBER', 1, (event) => {
     let n = partyMembers.length;
-    if (!n) {
-      return;
-    }
+    if (!n) { return; }
     for (let i = 0; i < n; i++) {
-      if (
-        partyMembers[i].playerId === event.playerId &&
-        partyMembers[i].serverId === event.serverId
-      ) {
+      if (partyMembers[i].playerId === event.playerId && partyMembers[i].serverId === event.serverId) {
         partyMembers[i].online = false;
       }
     }
+  });
+
+  mod.command.add('abb', () => {
+    enabled = !enabled;
+    mod.clearAllIntervals();
+    if (enabled) {
+      timerInterval = mod.setInterval(removeBodyBlock, 5000);
+    }
+    mod.command.message('Anti-bodyblock: ' + (enabled ? 'enabled.' : 'disabled.'));
   });
 };
